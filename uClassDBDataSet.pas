031d876be6820f187c94723c453963238f1766d0
Wagner Inaba <wagner.inaba@gmail.com>
2020-04-23 19:54:50 -0300

    Create property to convert data to json

--- uClassDBDataSet.pas
+++ uClassDBDataSet.pas
@@ -3,7 +3,7 @@ unit uClassDBDataSet;
 interface
 
 uses uClassDBConnection, Data.SqlExpr, Datasnap.Provider, Datasnap.DBClient,
-  Data.DB, System.Generics.Collections;
+  Data.DB, System.Generics.Collections, uClassDBToJson;
 
 type
   TBaseDBDataSet = Class
@@ -13,14 +13,18 @@ type
     dspBase : TDataSetProvider;
     cdsBase : TClientDataSet;
     dsBase  : TDataSource;
+    sJson   : TBaseDBToJson;
 
     FCommandText: String;
     FActivated: boolean;
     FDataSource: TDataSource;
     FDictParams : TDictionary<String, String>;
+    FDataJson: String;
     procedure SetCommandText(const Value: String);
     procedure SetActivated(const Value: boolean);
     procedure SetDataSource(const Value: TDataSource);
+    function LoadData(value : TDataSet) : String ;
+    procedure SetDataJson(const Value: String);
 
   public
     constructor Create;
@@ -28,6 +32,7 @@ type
     property CommandText : String read FCommandText write SetCommandText;
     property Activated : boolean read FActivated write SetActivated;
     property DataSource : TDataSource read FDataSource write SetDataSource;
+    property DataJson : String read FDataJson write SetDataJson;
     procedure ParamByName(name : string; value : variant);
 
 
@@ -82,6 +87,15 @@ begin
   FreeAndNil(dsBase);
 end;
 
+function TBaseDBDataSet.LoadData(value: TDataSet): String;
+begin
+
+  sJson := TBaseDBToJson.Create;
+
+  result := sJson.convert(value);
+
+end;
+
 procedure TBaseDBDataSet.ParamByName(name : string; value : variant);
 begin
   cdsbase.ParamByName(name).Value := value;
@@ -91,12 +105,20 @@ procedure TBaseDBDataSet.SetActivated(const Value: boolean);
 begin
   FActivated := Value;
   cdsBase.Active := FActivated;
+  SetDataJson(LoadData(cdsBase));
 end;
 
 procedure TBaseDBDataSet.SetCommandText(const Value: String);
 begin
   FCommandText := Value;
   cdsBase.CommandText := FCommandText;
+end;
+
+
+procedure TBaseDBDataSet.SetDataJson(const Value: String);
+
+begin
+  FDataJson := Value;
 end;
 
 procedure TBaseDBDataSet.SetDataSource(const Value: TDataSource);
